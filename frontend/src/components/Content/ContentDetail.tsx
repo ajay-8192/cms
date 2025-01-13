0c5f571e28eaaafa240c080c51982b7af1fc5962
ajay-8192 <ajaychowdary3699@gmail.com>
2025-01-13 21:33:32 +0530

    content data displaty on page level

--- frontend/src/components/Content/ContentDetail.tsx
+++ frontend/src/components/Content/ContentDetail.tsx
@@ -16,7 +16,6 @@ const ContentDetail: React.FC<ContentDetailProps> = ({ contentData }) => {
                     type: ""
                 }
             })
-            console.log('====> ', { d });
             setContent([...d]);
         } else {
             setContent([]);
@@ -27,12 +26,39 @@ const ContentDetail: React.FC<ContentDetailProps> = ({ contentData }) => {
         return null
     }
 
-    const handleOnKeyChange = (value: any, index: number) => {
-
+    const handleOnKeyChange = (key: any, index: number) => {
+        let updatedContents = [...content];
+        updatedContents = updatedContents.map((content, i) => {
+            
+            return {
+                ...content,
+                key: i === index ? key : content.key
+            }
+        });
+        setContent(updatedContents)
     };
+
     const handleOnValueChange = (value: any, index: number) => {
-
+        let updatedContents = [...content];
+        updatedContents = updatedContents.map((content, i) => {
+            return {
+                ...content,
+                value: i === index ? value : content.value
+            }
+        });
+        setContent(updatedContents)
     };
+
+    const handleNewContentData = () => {
+        setContent([
+            ...content,
+            {
+                key: "",
+                value: "",
+                type: ""
+            }
+        ])
+    }
 
     return (
         <div>
@@ -49,6 +75,7 @@ const ContentDetail: React.FC<ContentDetailProps> = ({ contentData }) => {
                 ))}
                 <button
                     className="w-fit border px-12 py-2 rounded-lg border-slate-900 font-bold bg-slate-800 text-white active:bg-white active:text-slate-800"
+                    onClick={handleNewContentData}
                 >
                     New Data
                 </button>
